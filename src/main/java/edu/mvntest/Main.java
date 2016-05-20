0f9ba0efe5bf54789bd33118daeceb79d246b3a0
sgaikwad <gaikwad.swapnil@gmail.com>
2016-05-20 11:37:39 +0100

    Added jodaTime library dependent code

--- src/main/java/edu/mvntest/Main.java
+++ src/main/java/edu/mvntest/Main.java
@@ -1,10 +1,18 @@
 package edu.mvntest;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 public class Main{
 
 
    public static void main(String[] args){
      System.out.println("Hello World!!!");
+
+     DateTime localTime = new DateTime();
+     System.out.println("Current time : " + localTime.toLocalTime() + " TimeZone: " + localTime.getZone());
+
+     DateTime usTime = localTime.toDateTime(DateTimeZone.forID("America/New_York"));
+     System.out.println("American time : " + usTime.toLocalTime() + " TimeZone: " + usTime.getZone());
    }
 }
